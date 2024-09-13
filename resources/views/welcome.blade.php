fa44e8525efdfbd9a30dd889a67a5f63151f0893
Marcelo Sarinho <marceloactsarinho@gmail.com>
2024-09-12 23:08:38 -0300

    navbar e modo tela cheia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,43 +14,49 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=SUSE:wght@100..800&display=swap" rel="stylesheet">
     </head>
-    <body class="h-screen font-suse flex flex-col items-center justify-center">
-        <button class="px-3 py-1.5 bg-blue-300 text-white">Adicionar</button>
-        <h1 class="text-3xl font-bold mb-4">
-            Kanban
-        </h1>
+    <body class="h-screen font-suse bg-gray-500">
+        <nav class="h-12 bg-red-200">
+            <button>Dark mode</button>
+            <button id="btn-fullscreen">Fullscreen</button>
+        </nav>
+        <main class="flex flex-col items-center justify-center">
+            <button class="px-3 py-1.5 bg-blue-300 text-white">Adicionar</button>
+            <h1 class="text-3xl font-bold mb-4">
+                Kanban
+            </h1>
 
-        <main class="flex gap-4">
-            <div class="col-lg-3 h-100">
-                <header>
-                    Coluna 1
-                </header>
-                <div>
-                    Card do kanban vem aqui
+            <div class="flex gap-4">
+                <div class="col-lg-3 h-100">
+                    <header class="text-2xl font-bold">
+                        A fazer
+                    </header>
+                    <div>
+                        Card do kanban vem aqui
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-3 h-100">
-                <header>
-                    Coluna 2
-                </header>
-                <div>
-                    Card do kanban vem aqui
+                <div class="col-lg-3 h-100">
+                    <header class="text-2xl font-bold">
+                        Em progresso
+                    </header>
+                    <div>
+                        Card do kanban vem aqui
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-3 h-100">
-                <header>
-                    Coluna 3
-                </header>
-                <div>
-                    Card do kanban vem aqui
+                <div class="col-lg-3 h-100">
+                    <header class="text-2xl font-bold">
+                        Testando
+                    </header>
+                    <div>
+                        Card do kanban vem aqui
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-3 h-100">
-                <header>
-                    Coluna 4
-                </header>
-                <div>
-                    Card do kanban vem aqui
+                <div class="col-lg-3 h-100">
+                    <header class="text-2xl font-bold">
+                        Implementado
+                    </header>
+                    <div>
+                        Card do kanban vem aqui
+                    </div>
                 </div>
             </div>
         </main>
@@ -60,6 +66,20 @@
     <script>
         $(document).ready(function () {
             console.log("jquery instalado");
+        });
+
+        $('#btn-fullscreen').on('click', function () {
+            toggleFullScreen();
         })
+
+        function toggleFullScreen() {
+            const bodyElement = document.querySelector('body');
+
+            if (!document.fullscreenElement) {
+                document.documentElement.requestFullscreen();
+            } else if (document.exitFullscreen) {
+                document.exitFullscreen();
+            }
+        }
     </script>
 </html>
