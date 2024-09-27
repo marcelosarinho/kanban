4ccbe03ccda6e65007de35ecc6c2cd2844ee8499
Marcelo Sarinho <marceloactsarinho@gmail.com>
2024-09-26 22:08:59 -0300

    criacao geral de colunas (controller, model, etc)

---
+++ app/Http/Controllers/ColumnController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreColumnRequest;
+use App\Http\Requests\UpdateColumnRequest;
+use App\Models\Column;
+
+class ColumnController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreColumnRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Column $column)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Column $column)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateColumnRequest $request, Column $column)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Column $column)
+    {
+        //
+    }
+}
