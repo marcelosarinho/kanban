1bff21a1874e6552d443df46c10b3fc3db19c593
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-01-19 02:57:18 -0300

    style: sidebar card

--- resources/views/components/sidebar-card.blade.php
+++ resources/views/components/sidebar-card.blade.php
@@ -1,4 +1,5 @@
-<div class="bg-lime-400 py-2 px-3 rounded cursor-pointer border-2 border-black dark:bg-dark-lime">
+<div class="bg-gray-100 py-2 px-3 rounded cursor-pointer border border-gray-300
+  dark:bg-zinc-700 dark:border-zinc-900 dark:text-gray-300">
   <h2 class="font-semibold truncate">{{ $name }}</h2>
   <h6 class="text-sm truncate leading-tight">{{ $description }}</h6>
 </div>
