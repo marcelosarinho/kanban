8dba34a05d58b4fee69dc35a8b594d873349b99c
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-01-28 21:43:27 -0300

    style: focus-visible ring size

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -134,17 +134,17 @@
         <form id="create-project-form">
             @csrf
             <div class="mb-3">
-                <label class="w-fit block mb-1 dark:text-gray-300" for="name">Nome</label>
+                <label class="w-fit block mb-2 dark:text-gray-300" for="name">Nome</label>
                 <input name="name" id="name" type="text" class="px-2 py-1.5 border border-gray-300 rounded w-full dark:bg-slate-800 dark:border-slate-950
-                    focus-visible:ring-[3px] focus-visible:ring-primary/40 focus-visible:outline-none focus-visible:border
+                    focus-visible:ring-4 focus-visible:ring-primary/40 focus-visible:outline-none focus-visible:border
                     focus-visible:border-primary dark:text-gray-300" />
                 <p id="name-error" class="text-red-500 text-sm mt-1 hidden">Nome obrigatório!</p>
             </div>
 
             <div class="mb-3">
-                <label class="w-fit block mb-1 dark:text-gray-300" for="description">Descrição</label>
+                <label class="w-fit block mb-2 dark:text-gray-300" for="description">Descrição</label>
                 <textarea name="description" id="description" cols="30" rows="3"
-                    class="px-2 py-1.5 border border-gray-300 rounded w-full dark:bg-slate-800 dark:border-slate-950 focus-visible:ring-[3px]
+                    class="px-2 py-1.5 border border-gray-300 rounded w-full dark:bg-slate-800 dark:border-slate-950 focus-visible:ring-4
                     focus-visible:ring-primary/40 focus-visible:outline-none focus-visible:border focus-visible:border-primary
                     dark:text-gray-300"
                 ></textarea>
