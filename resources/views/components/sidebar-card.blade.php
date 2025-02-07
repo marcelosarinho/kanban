6ce4b728cdabdbfe05304d17175dc82c4a4fa90b
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-02-07 00:20:14 -0300

    style: delete button and border on hover on sidebar card

--- resources/views/components/sidebar-card.blade.php
+++ resources/views/components/sidebar-card.blade.php
@@ -1,5 +1,11 @@
-<div class="bg-gray-100 py-2 px-3 rounded cursor-pointer border border-gray-300
-  dark:bg-slate-800 dark:border-slate-700 dark:text-gray-300">
+<div
+  class="bg-gray-100 py-2 px-3 rounded border border-gray-300
+dark:bg-slate-800 dark:border-slate-700 dark:text-gray-300
+  hover:border-primary transition-colors"
+>
   <h2 class="font-semibold truncate">{{ $name }}</h2>
   <h6 class="text-sm truncate leading-tight">{{ $description }}</h6>
+  <button class="group flex mt-2">
+    <i class="ph ph-trash text-xl text-danger group-hover:text-red-600 transition-colors"></i>
+  </button>
 </div>
