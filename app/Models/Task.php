4d8e96c8fe7ffaaa457dcec578382e5eabacfa90
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-05-17 18:04:02 -0300

    feat: add fillable to task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'priority',
+        'category',
+        'progress',
+        'commentary',
+        'project_id',
+        'color',
+        'done',
+        'status',
+    ];
 }
