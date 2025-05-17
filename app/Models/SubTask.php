c0be4212e1980fc4f3a3b18b38c041d947392a38
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-05-17 17:59:51 -0300

    feat: create subtask model

--- app/Models/SubTask.php
+++ app/Models/SubTask.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Column extends Model
+class SubTask extends Model
 {
     use HasFactory;
 }
