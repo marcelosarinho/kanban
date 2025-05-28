8f8499104f0025ffa0bf743933190d01ae981c73
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-05-28 19:04:26 -0300

    style: modal animation in error and success modal

--- resources/views/components/error-modal.blade.php
+++ resources/views/components/error-modal.blade.php
@@ -1,6 +1,6 @@
 <div id="error-modal" class="z-[60] hidden absolute bg-black/35 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 size-full">
   <div class="flex flex-col items-center w-1/2 bg-white absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
-    gap-y-6 rounded p-5 dark:bg-slate-900 dark:text-gray-300">
+    gap-y-6 rounded p-5 dark:bg-slate-900 dark:text-gray-300 animate-in">
     <i class="ph-fill ph-x-circle text-8xl text-danger"></i>
     <header class="text-3xl font-medium">
       Erro!
--- resources/views/components/success-modal.blade.php
+++ resources/views/components/success-modal.blade.php
@@ -1,6 +1,6 @@
 <div id="success-modal" {{ $attributes->merge(['class' => 'hidden absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 size-full bg-black/35']) }}>
   <div class="w-1/2 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-5 rounded flex flex-col
-    items-center gap-y-6 dark:bg-slate-900 dark:text-gray-300">
+    items-center gap-y-6 dark:bg-slate-900 dark:text-gray-300 animate-in">
     <i class="ph-fill ph-check-fat text-8xl text-success"></i>
     <header class="text-3xl font-medium">
       Sucesso!
