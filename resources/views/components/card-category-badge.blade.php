967192e2670166097825ec68908b254fb42fc359
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-06-12 20:21:17 -0300

    feat: card component

---
+++ resources/views/components/card-category-badge.blade.php
@@ -0,0 +1,3 @@
+<div class="h-fit flex items-center text-xs font-semibold bg-blue-100 text-blue-700 border-[1.5px] rounded-full px-3 py-0.5 border-blue-300">
+    {{ $label }}
+</div>
--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,15 +1,8 @@
-<div class="p-4 border border-l-4 border-blue-500 rounded-md bg-white dark:bg-slate-800 dark:border-blue-700 dark:text-gray-300">
-    <div class="flex items-center gap-1 mb-3">
-		<input class="size-4 border border-red-500" type="checkbox" name="teste" id="teste">
-		<label for="teste">Concluída</label>
-	</div>
-	
-	<header class="flex justify-between items-center">
-        <div
-            class="h-fit flex items-center text-xs font-semibold bg-blue-100 text-blue-700 border-[1.5px] rounded-full px-3 py-0.5 border-blue-300">
-            Badge</div>
-        <div
-            class="border rounded py-1.5 px-2 border-dashed border-gray-300 hover:cursor-pointer hover:bg-gray-100 transition-colors">
+<div
+    class="p-4 border border-l-4 border-blue-500 rounded-md bg-white dark:bg-slate-800 dark:border-blue-700 dark:text-gray-300">
+    <header class="flex justify-between items-center">
+        <x-card-category-badge label="Badge" />
+        <div class="border rounded py-1.5 px-2 border-dashed border-gray-300 hover:cursor-pointer hover:bg-gray-100 transition-colors">
             <div class="size-3 bg-blue-500 rounded-full"></div>
         </div>
     </header>
@@ -19,13 +12,17 @@
         <p class="text-sm leading-tight">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, optio.</p>
     </div>
 
-	<div class="flex justify-end my-3">
-		<select class="text-sm border rounded p-0.5" name="priority" id="priority">
-			<option value="low">Baixa</option>
-			<option value="medium">Média</option>
-			<option value="high">Alta</option>
-		</select>
-	</div>
+    <div class="flex justify-between items-end my-3">
+        <div class="flex items-center gap-1">
+            <input class="size-4 border border-red-500" type="checkbox" name="teste" id="teste">
+            <label class="text-sm" for="teste">Concluída</label>
+        </div>
+        <select class="text-sm border rounded p-0.5" name="priority" id="priority">
+            <option value="low">Baixa</option>
+            <option value="medium">Média</option>
+            <option value="high">Alta</option>
+        </select>
+    </div>
 
     <div class="my-3">
         <div class="flex justify-between">
