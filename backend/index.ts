54f10837be7f751b9e6ef5a265273168f3ca8861
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-07-10 01:01:58 -0300

    feat: backend scaffolding

---
+++ backend/index.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+
+const server = fastify();
+
+server.get("/ping", () => {
+    return "pong\n";
+});
+
+server.listen({port: 8080}, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log(`Server running at ${address}`)
+});
