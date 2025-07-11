58a01273f21cabe2260cc33c5a7413fd9f77930e
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-07-11 10:44:18 -0300

    feat: install tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,11 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  async function callServer() {
-    await fetch('http://localhost:8080/ping')
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error(error))
-  }
-
-  useEffect(() => {
-    callServer()
-  }, [])
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='bg-red-500 w-10 h-10'>
+        Oi
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
