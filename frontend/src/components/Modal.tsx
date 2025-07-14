27efb5b7a7e02fd5bf2bf911e5f3eb6a775a0cc9
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-07-14 12:38:14 -0300

    style: modal size

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -5,7 +5,7 @@ export default function Modal(props: ComponentProps<'dialog'>) {
 
   return (
       <dialog className="fixed bg-black/35 z-50 size-full" id={id} {...rest}>
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white border border-gray-300 p-5 rounded dark:bg-slate-900 dark:border-slate-700">
+        <div className="absolute w-1/2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white border border-gray-300 p-5 rounded dark:bg-slate-900 dark:border-slate-700">
           {children}
         </div>
       </dialog>
