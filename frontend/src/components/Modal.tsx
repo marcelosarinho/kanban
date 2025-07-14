7d8ce3a3d9e605f886ef4948ab1815261adf6829
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-07-14 12:51:30 -0300

    refactor: removed unnecessary id from modal props

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,10 +1,10 @@
 import type { ComponentProps } from "react";
 
 export default function Modal(props: ComponentProps<'dialog'>) {
-  const { children, id, ...rest } = props;
+  const { children, ...rest } = props;
 
   return (
-      <dialog className="fixed bg-black/35 z-50 size-full" id={id} {...rest}>
+      <dialog className="fixed bg-black/35 z-50 size-full" {...rest}>
         <div className="absolute w-1/2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white border border-gray-300 p-5 rounded dark:bg-slate-900 dark:border-slate-700">
           {children}
         </div>
