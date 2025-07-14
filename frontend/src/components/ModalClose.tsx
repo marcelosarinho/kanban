1501c76b1f6eba69773e4a32829b32c053c7ad50
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-07-14 11:33:38 -0300

    style: changed text-white to text-gray-300 on dark mode and modal close styles

--- frontend/src/components/ModalClose.tsx
+++ frontend/src/components/ModalClose.tsx
@@ -4,7 +4,11 @@ export default function ModalClose(props: ComponentProps<'button'>) {
   const { ...rest } = props;
 
   return (
-    <button {...rest}>
+    <button
+      type="button"
+      className="flex items-center justify-center rounded-full cursor-pointer p-1 transition-colors hover:bg-gray-200 dark:hover:bg-slate-800 dark:text-gray-300"
+      {...rest}
+    >
       <i className="ph ph-x"></i>
     </button>
   )
