de7cfb57c32b4113ab5d32af8545602dc218f300
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-07-15 17:19:39 -0300

    style: progress bar color

--- frontend/src/components/ProgressBar.tsx
+++ frontend/src/components/ProgressBar.tsx
@@ -7,7 +7,7 @@ export default function ProgressBar() {
       </div>
 
       <div className="w-full h-2 bg-gray-200 dark:bg-slate-700 rounded-full">
-        <div className="h-2 bg-green-500 dark:bg-green-700 rounded-full w-2/3"></div>
+        <div className="h-2 bg-success rounded-full w-2/3"></div>
       </div>
     </div>
   )
