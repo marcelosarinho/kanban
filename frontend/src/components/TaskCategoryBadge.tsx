c0140e779c67581f189e58cc6cdd8e196f91fffe
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-07-16 13:47:05 -0300

    refactor: changed variable categories to CATEGORIES in task category badge component

--- frontend/src/components/TaskCategoryBadge.tsx
+++ frontend/src/components/TaskCategoryBadge.tsx
@@ -1,22 +1,15 @@
-const categories = {
-  frontend: { color: 'badge-blue', name: 'Front-end' },
-  backend: { color: 'badge-green', name: 'Back-end' },
-  mobile: { color: 'badge-red', name: 'Mobile' },
-  ui_ux: { color: 'badge-purple', name: 'UI/UX' },
-  devops: { color: 'badge-pink', name: 'DevOps' },
-  database: { color: 'badge-orange', name: 'Banco de dados' },
-}
+import { CATEGORIES } from "../libs/constants";
 
 type TaskCategoryBadgeProps = {
-  category: keyof typeof categories;
+  category: keyof typeof CATEGORIES;
 }
 
 export default function TaskCategoryBadge(props: TaskCategoryBadgeProps) {
   const { category } = props;
 
   return (
-    <div className={`task ${categories[category].color} h-fit flex items-center text-xs font-semibold border-[1.5px] rounded-full px-3 py-0.5`}>
-      {categories[category].name}
+    <div className={`task ${CATEGORIES[category].color} h-fit flex items-center text-xs font-semibold border-[1.5px] rounded-full px-3 py-0.5`}>
+      {CATEGORIES[category].name}
     </div>
   )
 }
