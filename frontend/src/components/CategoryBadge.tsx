51a12ae4100bd2a694f8e856493723b5e1662def
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-07-16 18:11:28 -0300

    style: select-none in category badge

--- frontend/src/components/CategoryBadge.tsx
+++ frontend/src/components/CategoryBadge.tsx
@@ -9,7 +9,7 @@ export default function CategoryBadge(props: CategoryBadgeProps) {
   const { category, disabled, ...rest } = props;
 
   return (
-    <label className={`dark:bg-slate-800 dark:border-slate-950 dark:text-gray-300 has-[:disabled]:opacity-50 border-[1.5px] transition-colors border-gray-300 rounded-full py-1 px-2 cursor-pointer ${CATEGORIES[category].color}`}>
+    <label className={`select-none dark:bg-slate-800 dark:border-slate-950 dark:text-gray-300 has-[:disabled]:opacity-50 border-[1.5px] transition-colors border-gray-300 rounded-full py-1 px-2 cursor-pointer ${CATEGORIES[category].color}`}>
       <input disabled={disabled} className="hidden" type="checkbox" {...rest} />
       {CATEGORIES[category].name}
     </label>
