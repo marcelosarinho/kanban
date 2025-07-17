6c68935b8311d21f2317e2591988f3b3a6faf9b2
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-07-17 17:48:11 -0300

    feat: create projects table migration

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,8 +1,10 @@
+import { timestamp } from "drizzle-orm/pg-core";
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
+export const projectsTable = pgTable('projects', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
+  name: varchar({ length: 255 }).notNull().unique(),
+  description: varchar({ length: 255 }).notNull(),
+  created_at: timestamp('created_at', { mode: 'string' }).defaultNow().notNull(),
+  updated_at: timestamp('updated_at', { mode: 'string' }).defaultNow().notNull(),
+})
