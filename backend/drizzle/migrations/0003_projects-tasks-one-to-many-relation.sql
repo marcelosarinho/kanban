17a2278610793ee4be6b95e2a98e12ac71eb51cb
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-07-18 12:33:04 -0300

    feat: added foreign key in database level for projects and tasks relation

---
+++ backend/drizzle/migrations/0003_projects-tasks-one-to-many-relation.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "tasks" ADD COLUMN "project_id" integer NOT NULL;--> statement-breakpoint
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_project_id_projects_id_fk" FOREIGN KEY ("project_id") REFERENCES "public"."projects"("id") ON DELETE cascade ON UPDATE no action;
--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import { boolean, pgEnum, real, timestamp } from "drizzle-orm/pg-core";
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
@@ -12,6 +13,10 @@ export const projectsTable = pgTable('projects', {
   updatedAt: timestamp('updated_at', { mode: 'string' }).defaultNow().notNull(),
 })
 
+export const projectsRelations = relations(projectsTable, ({ many }) => ({
+  tasks: many(tasksTable),
+}));
+
 export const tasksTable = pgTable('tasks', {
   id: integer().primaryKey().generatedByDefaultAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
@@ -23,9 +28,17 @@ export const tasksTable = pgTable('tasks', {
   color: varchar({ length: 20 }),
   done: boolean().notNull().default(false),
   status: statusEnum().notNull(),
+  projectId: integer('project_id').notNull().references(() => projectsTable.id, { onDelete: 'cascade' }),
   createdAt: timestamp('created_at', { mode: 'string' }).defaultNow().notNull(),
   updatedAt: timestamp('updated_at', { mode: 'string' }).defaultNow().notNull(),
 })
+
+export const tasksRelations = relations(tasksTable, ({ one }) => ({
+  projects: one(projectsTable, {
+    fields: [tasksTable.projectId],
+    references: [projectsTable.id],
+  }),
+}));
 
 export const subtasksTable = pgTable('subtasks', {
   id: integer().primaryKey().generatedByDefaultAsIdentity(),
