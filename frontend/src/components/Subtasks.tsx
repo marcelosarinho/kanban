c864e3435c213a45e16680cc23b65a4428af4506
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-07-21 17:55:28 -0300

    feat: added caret down icon in substasks component

--- frontend/src/components/Subtasks.tsx
+++ frontend/src/components/Subtasks.tsx
@@ -1,10 +1,11 @@
-import { CaretRightIcon, CheckIcon } from "@phosphor-icons/react";
+import { CaretDownIcon, CaretRightIcon, CheckIcon } from "@phosphor-icons/react";
 
 export default function Subtasks() {
   return (
     <>
       <div className="flex gap-1.5 items-center hover:bg-gray-100 dark:hover:bg-slate-700 p-1 rounded transition-colors cursor-pointer">
           <CaretRightIcon weight="bold" className="text-sm"/>
+          <CaretDownIcon weight="bold" className="text-sm"/>
           <span className="text-sm">Subtarefas (2/4)</span>
         </div>
 
