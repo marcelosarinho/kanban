0f52aa2179c827f91cb7b905ef58f06b53c25ae0
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-07-30 14:29:44 -0300

    feat: createProject in api folder

--- frontend/src/api/projects.ts
+++ frontend/src/api/projects.ts
@@ -1,3 +1,5 @@
+import type { Project } from "../types/project";
+
 export async function getProjects() {
   try {
     const response = await fetch('http://localhost:8080/projects');
@@ -6,5 +8,23 @@ export async function getProjects() {
     return data;
   } catch (error) {
     console.error('Erro ao buscar projetos!', error);
+  }
+}
+
+export async function createProject(project: Project) {
+  try {
+    const response = await fetch('http://localhost:8080/projects', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(project)
+    })
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error('Erro ao criar projeto!', error);
   }
 }
