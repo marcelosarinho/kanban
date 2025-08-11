81abaaf0e334bf7dc00ca8a198c9972338b164f5
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-08-11 12:11:27 -0300

    fix: custom tooltips weren't showing if projects name/description used space

--- frontend/src/components/SidebarCard.tsx
+++ frontend/src/components/SidebarCard.tsx
@@ -2,6 +2,7 @@ import { PencilSimpleIcon, TrashIcon } from "@phosphor-icons/react";
 import Button from "./Button";
 import CustomTooltip from "./CustomTooltip";
 import type { Project } from "../types/project";
+import { addUnderscoresToText } from "../utils/functions";
 
 type SidebarCardProps = {
   project: Project;
@@ -11,22 +12,25 @@ type SidebarCardProps = {
 export default function SidebarCard(props: SidebarCardProps) {
   const { project, openModal } = props;
 
+  const underscoredProjectName = addUnderscoresToText(project.name);
+  const underscoredProjectDescription = addUnderscoresToText(project.description);
+
   return (
     <div className="bg-gray-100 py-2 px-3 rounded border border-gray-300 dark:bg-slate-800 dark:border-slate-700 dark:text-gray-300 hover:border-primary transition-colors">
-      <h2 id={project.name} className="w-fit font-semibold truncate">{project.name}</h2>
-      <CustomTooltip anchorSelect={`#${project.name}`} content={project.name} />
+      <h2 id={underscoredProjectName} className="w-fit font-semibold truncate">{project.name}</h2>
+      <CustomTooltip anchorSelect={`#${underscoredProjectName}`} content={project.name} />
 
-      <h6 id={project.description} className="w-fit text-sm truncate leading-tight">{project.description}</h6>
-      <CustomTooltip anchorSelect={`#${project.description}`} content={project.description} />
+      <h6 id={underscoredProjectDescription} className="w-fit text-sm truncate leading-tight">{project.description}</h6>
+      <CustomTooltip anchorSelect={`#${underscoredProjectDescription}`} content={project.description} />
 
       <div className="flex items-center justify-end">
-        <Button id={`edit_${project.name}`} variant="transparent" onClick={() => openModal('create-project-modal', project, true)}>
+        <Button id={`edit_${underscoredProjectName}`} variant="transparent" onClick={() => openModal('create-project-modal', project, true)}>
           <PencilSimpleIcon weight="bold" className="text-lg text-info transition-colors" />
-          <CustomTooltip anchorSelect={`#edit_${project.name}`} content="Editar" />
+          <CustomTooltip anchorSelect={`#edit_${underscoredProjectName}`} content="Editar" />
         </Button>
-        <Button id={`delete_${project.name}`} variant="transparent" onClick={() => openModal('delete-project-modal', project)}>
+        <Button id={`delete_${underscoredProjectName}`} variant="transparent" onClick={() => openModal('delete-project-modal', project)}>
           <TrashIcon weight="bold" className="text-lg text-danger transition-colors" />
-          <CustomTooltip anchorSelect={`#delete_${project.name}`} content="Deletar" />
+          <CustomTooltip anchorSelect={`#delete_${underscoredProjectName}`} content="Deletar" />
         </Button>
       </div>
     </div>
