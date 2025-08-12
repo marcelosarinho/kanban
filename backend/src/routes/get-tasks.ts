ec648800ea5502acba4e1195b9021d6da79d2e4f
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-08-12 17:48:41 -0300

    feat: get tasks function backend (incomplete)

--- backend/src/routes/get-tasks.ts
+++ backend/src/routes/get-tasks.ts
@@ -2,15 +2,26 @@ import { FastifyInstance } from "fastify";
 import { db } from "..";
 import { eq } from "drizzle-orm";
 import { tasks } from "../db/schema";
+import { Task, TaskStatusOption } from "../types/task";
 
 export async function getTasks(app: FastifyInstance) {
   app.get('/projects/:id/tasks', async (request: any, reply: any) => {
     try {
       const { id } = request.params;
 
-      const results = await db.select().from(tasks).where(eq(tasks.projectId, id)).groupBy(tasks.status);
+      const results = await db.select().from(tasks).where(eq(tasks.projectId, id));
 
-      return reply.status(200).send(results);
+      const groupedTasks = results.reduce((acc, task) => {
+        if (!acc[task.status]) {
+          console.log(task.status);
+          acc[task.status] = [];
+        }
+
+        acc[task.status].push(task as unknown as Task);
+        return acc;
+      }, {} as Record<TaskStatusOption, Task[]>);
+
+      return reply.status(200).send(groupedTasks);
     } catch (error) {
       console.log(error);
     }
