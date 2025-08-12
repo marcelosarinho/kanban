82876e1afbbafa30f7b295e82e37027a4f9742a2
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-08-12 10:14:45 -0300

    feat: task type

--- frontend/src/types/task.ts
+++ frontend/src/types/task.ts
@@ -1,5 +1,17 @@
+import type { CategoryOption, Color, TaskPriorityOption, TaskStatusOption } from "./constants";
+
 export type Task = {
   id: string;
   name: string;
-  description: string;
+  description?: string;
+  priority: TaskPriorityOption;
+  category?: CategoryOption;
+  progress: number;
+  commentary?: string;
+  color?: Color;
+  done: boolean;
+  status: TaskStatusOption;
+  createdAt: string;
+  updatedAt: string;
+  projectId: string;
 }
