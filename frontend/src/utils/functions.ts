aff3cdb50ee7c94e7935a674f281285a471efec9
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-09-04 15:19:10 -0300

    feat: getCookie utility function

--- frontend/src/utils/functions.ts
+++ frontend/src/utils/functions.ts
@@ -6,4 +6,11 @@ export function setCookie(name: string, value: string) {
   document.cookie = `${name}=${value}; path=/; max-age=${60 * 60 * 24 * 30}`;
 }
 
+export function getCookie(name: string) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+
+  if (parts.length === 2) return parts.pop()?.split(';').shift();
+}
+
 // export function debounce<T extends (...args: any[]) => any>()
