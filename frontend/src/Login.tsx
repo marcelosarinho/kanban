8117f9f65e4e2b724ebf30640bd7e06d276fe8d2
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-09-04 10:50:00 -0300

    style: login/register/forgot password in light mode

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -10,9 +10,9 @@ export default function Login() {
   return (
     <main className="flex justify-center items-center h-screen">
       {formType === 'login' && (
-        <div className="border rounded-lg dark:bg-slate-900 dark:border-slate-700">
-          <div className="border-b dark:border-slate-700 p-6">
-            <h1 className="text-center dark:text-gray-300 text-2xl font-bold">Login</h1>
+        <div className="border rounded-lg bg-white border-neutral-300 dark:bg-slate-900 dark:border-slate-700 shadow-lg">
+          <div className="border-b border-neutral-300 dark:border-slate-700 p-6">
+            <h1 className="text-center dark:text-gray-300 text-2xl font-medium">Login</h1>
           </div>
           <form className="p-6" action="">
             <fieldset className="flex flex-col gap-4">
@@ -27,15 +27,15 @@ export default function Login() {
               <Button className="justify-center">Entrar</Button>
             </fieldset>
           </form>
-          <div className="p-6 border-t dark:border-slate-700">
+          <div className="p-6 border-t border-neutral-300 dark:border-slate-700">
             <p className="text-center dark:text-gray-300 text-sm">Não tem conta? <span onClick={() => setFormType('register')} className="text-primary cursor-pointer hover:text-primary/80 transition-colors">Cadastre-se de graça!</span></p>
           </div>
         </div>
       )}
       {formType === 'register' && (
-        <div className="border rounded-lg dark:bg-slate-900 dark:border-slate-700">
-          <div className="border-b dark:border-slate-700 p-6">
-            <h1 className="text-center dark:text-gray-300 text-2xl font-bold">Cadastre-se</h1>
+        <div className="border rounded-lg bg-white border-neutral-300 dark:bg-slate-900 dark:border-slate-700 shadow-lg">
+          <div className="border-b border-neutral-300 dark:border-slate-700 p-6">
+            <h1 className="text-center dark:text-gray-300 text-2xl font-medium">Cadastre-se</h1>
           </div>
           <form className="p-6" action="">
             <fieldset className="flex flex-col gap-4">
@@ -45,15 +45,15 @@ export default function Login() {
               <Button className="justify-center">Cadastrar</Button>
             </fieldset>
           </form>
-          <div className="p-6 border-t dark:border-slate-700">
+          <div className="p-6 border-t border-neutral-300 dark:border-slate-700">
             <p className="text-center dark:text-gray-300 text-sm">Já tem conta? <span onClick={() => setFormType('login')} className="text-primary cursor-pointer hover:text-primary/80 transition-colors">Faça login!</span></p>
           </div>
         </div>
       )}
       {formType === 'forgot-password' && (
-        <div className="border rounded-lg dark:bg-slate-900 dark:border-slate-700">
-          <div className="border-b dark:border-slate-700 p-6">
-            <h1 className="text-center dark:text-gray-300 text-2xl font-bold">Esqueceu a senha?</h1>
+        <div className="border rounded-lg bg-white border-neutral-300 dark:bg-slate-900 dark:border-slate-700 shadow-lg">
+          <div className="border-b border-neutral-300 dark:border-slate-700 p-6">
+            <h1 className="text-center dark:text-gray-300 text-2xl font-medium">Esqueceu a senha?</h1>
           </div>
           <form className="p-6" action="">
             <p className="text-center dark:text-gray-300 text-sm mb-4">Um email será enviado com as instruções necessárias para redefinir sua senha.</p>
@@ -62,7 +62,7 @@ export default function Login() {
               <Button className="justify-center">Recuperar</Button>
             </fieldset>
           </form>
-          <div className="p-6 border-t dark:border-slate-700">
+          <div className="p-6 border-t border-neutral-300 dark:border-slate-700">
             <p className="text-center dark:text-gray-300 text-sm">Lembrou sua senha? <span onClick={() => setFormType('login')} className="text-primary cursor-pointer hover:text-primary/80 transition-colors">Faça login!</span></p>
           </div>
         </div>
