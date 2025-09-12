669f53b6fa641b0965762cce78c14e0826c0e68f
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-09-12 11:31:47 -0300

    feat: register mutation

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -4,11 +4,13 @@ import Input from "./components/Input";
 import ThemeButton from "./components/ThemeButton";
 import { userForgotPasswordSchema, userLoginSchema, userRegisterSchema } from "./schemas/user";
 import type z from "zod";
-import { useForm } from "react-hook-form";
+import { useForm, type FieldErrors } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import UserFormError from "./components/UserFormError";
+import { useMutation } from "@tanstack/react-query";
+import { createUser } from "./api";
 
-type FormType = 'login' | 'register' | 'forgot-password';
+type FormType = 'login' | 'register' | 'forgot-password' | 'register-email-sent';
 
 type InputsLogin = z.infer<typeof userLoginSchema>;
 type InputsRegister = z.infer<typeof userRegisterSchema>;
@@ -41,8 +43,27 @@ export default function Login() {
     resolver: zodResolver(userForgotPasswordSchema),
   });
 
-  function onSubmit() {
-    console.log('foi');
+  const registerMutation = useMutation({
+    mutationFn: createUser,
+    onSuccess: () => {
+      setFormType('register-email-sent');
+    }
+  })
+
+  function onSubmitRegister(data: InputsRegister) {
+    registerMutation.mutate(data);
+  }
+
+  function onSubmitLogin(data: InputsLogin) {
+    console.log(data);
+  }
+
+  function onSubmitForgotPassword(data: InputsForgotPassword) {
+    console.log(data);
+  }
+
+  function onError(errors: FieldErrors<InputsRegister>) {
+    console.log(errors)
   }
 
   return (
@@ -54,7 +75,7 @@ export default function Login() {
           <div className="border-b border-neutral-300 dark:border-slate-700 p-6">
             <h1 className="animate-slide-in-from-bottom text-center dark:text-gray-300 text-2xl font-medium">Login</h1>
           </div>
-          <form onSubmit={handleLoginSubmit(onSubmit)} className="p-6">
+          <form onSubmit={handleLoginSubmit(onSubmitLogin)} className="p-6">
             <fieldset className="flex flex-col gap-4">
               <UserFormError error="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis rerum quisquam quam incidunt repellat. Quis soluta, quas commodi accusamus excepturi aspernatur, quod recusandae perferendis error nesciunt voluptatibus omnis dolorem nostrum eius, odio porro suscipit hic animi repellat aliquid eaque? Quo expedita eligendi rerum sed consequuntur aliquid, odit repellat recusandae tenetur?" />
               <Input error={loginErrors.email?.message} {...registerLogin('email')} className="animate-slide-in-from-bottom" label="Email" type="email" name="email" id="email" />
@@ -79,14 +100,16 @@ export default function Login() {
           <div className="border-b border-neutral-300 dark:border-slate-700 p-6">
             <h1 className="animate-slide-in-from-bottom text-center dark:text-gray-300 text-2xl font-medium">Cadastre-se</h1>
           </div>
-          <form onSubmit={handleRegisterSubmit(onSubmit)} className="p-6">
+          <form onSubmit={handleRegisterSubmit(onSubmitRegister, onError)} className="p-6">
             <fieldset className="flex flex-col gap-4">
-              <UserFormError error="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis rerum quisquam quam incidunt repellat. Quis soluta, quas commodi accusamus excepturi aspernatur, quod recusandae perferendis error nesciunt voluptatibus omnis dolorem nostrum eius, odio porro suscipit hic animi repellat aliquid eaque? Quo expedita eligendi rerum sed consequuntur aliquid, odit repellat recusandae tenetur?" />
+              {registerMutation.isError && (
+                <UserFormError error="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis rerum quisquam quam incidunt repellat. Quis soluta, quas commodi accusamus excepturi aspernatur, quod recusandae perferendis error nesciunt voluptatibus omnis dolorem nostrum eius, odio porro suscipit hic animi repellat aliquid eaque? Quo expedita eligendi rerum sed consequuntur aliquid, odit repellat recusandae tenetur?" />
+              )}
               <Input error={registerErrors.name?.message} {...registerRegister('name')} className="animate-slide-in-from-bottom" label="Nome" type="text" name="name" id="name" />
               <Input error={registerErrors.email?.message} {...registerRegister('email')} className="animate-slide-in-from-bottom" label="Email" type="email" name="email" id="email" />
               <Input error={registerErrors.password?.message} {...registerRegister('password')} className="animate-slide-in-from-bottom" label="Senha" type="password" name="password" id="password" isPassword />
               <Input error={registerErrors.password_confirmation?.message} {...registerRegister('password_confirmation')} className="animate-slide-in-from-bottom" label="Confirmar senha" type="password" name="password_confirmation" id="password_confirmation" isPassword />
-              <Button className="animate-slide-in-from-bottom justify-center">Cadastrar</Button>
+              <Button type="submit" className="animate-slide-in-from-bottom justify-center">Cadastrar</Button>
             </fieldset>
           </form>
           <div className="p-6 border-t border-neutral-300 dark:border-slate-700">
@@ -100,7 +123,7 @@ export default function Login() {
           <div className="border-b border-neutral-300 dark:border-slate-700 p-6">
             <h1 className="animate-slide-in-from-bottom text-center dark:text-gray-300 text-2xl font-medium">Esqueceu a senha?</h1>
           </div>
-          <form onSubmit={handleForgotPasswordSubmit(onSubmit)} className="p-6">
+          <form onSubmit={handleForgotPasswordSubmit(onSubmitForgotPassword)} className="p-6">
             <p className="animate-slide-in-from-bottom text-center dark:text-gray-300 text-sm mb-4">Um email será enviado com as instruções necessárias para redefinir sua senha.</p>
             <fieldset className="flex flex-col gap-4">
               <Input error={forgotPasswordErrors.email?.message} {...registerForgotPassword('email')} className="animate-slide-in-from-bottom" label="Email" type="email" name="email" id="email" />
@@ -109,6 +132,17 @@ export default function Login() {
           </form>
           <div className="p-6 border-t border-neutral-300 dark:border-slate-700">
             <p className="animate-slide-in-from-bottom text-center dark:text-gray-300 text-sm">Lembrou sua senha? <span onClick={() => setFormType('login')} className="text-primary cursor-pointer hover:text-primary/80 transition-colors">Faça login!</span></p>
+          </div>
+        </div>
+      )}
+
+      {formType === 'register-email-sent' && (
+        <div className="animate-in min-w-1/2 max-w-sm lg:min-w-1/3 lg:max-w-lg xl:min-w-1/4 xl:max-w-xl border rounded-lg bg-white border-neutral-300 dark:bg-slate-900 dark:border-slate-700 shadow-lg">
+          <div className="border-b border-neutral-300 dark:border-slate-700 p-6">
+            <h1 className="animate-slide-in-from-bottom text-center dark:text-gray-300 text-2xl font-medium">Email enviado</h1>
+          </div>
+          <div className="p-6 border-t border-neutral-300 dark:border-slate-700">
+            <p className="animate-slide-in-from-bottom text-center dark:text-gray-300 text-sm">Um email foi enviado para o seu email com as instruções necessárias para redefinir sua senha.</p>
           </div>
         </div>
       )}
