1a2e0dfe72d29c05fcdd569383a28cefc2405c3d
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-09-16 16:08:02 -0300

    style: add warning color variable

--- frontend/src/components/UserFormMessage.tsx
+++ frontend/src/components/UserFormMessage.tsx
@@ -7,7 +7,7 @@ type UserFormMessageProps = {
 
 const userFormVariant: Record<Variant, string> = {
   error: 'bg-danger/20 border-danger text-red-800 dark:text-red-400',
-  warning: 'bg-yellow-500/20 border-yellow-500 text-yellow-800 dark:text-yellow-400',
+  warning: 'bg-warning/20 border-warning text-yellow-800 dark:text-yellow-400',
   success: 'bg-success/20 border-success text-green-800 dark:text-green-400',
 }
 
