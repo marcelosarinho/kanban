24e09c703ace79427d0526be96d32d54f28b5aed
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-09-17 10:33:28 -0300

    feat: login card component

---
+++ frontend/src/components/LoginCard.tsx
@@ -0,0 +1,14 @@
+import type { ComponentProps } from "react";
+
+export default function LoginCard(props: ComponentProps<'div'>) {
+  const { children, ...rest } = props;
+
+  return (
+    <div
+      {...rest}
+      className="animate-in min-w-1/2 max-w-sm lg:min-w-1/3 lg:max-w-lg xl:min-w-1/4 xl:max-w-xl border rounded-lg bg-white border-neutral-300 dark:bg-slate-900 dark:border-slate-700 shadow-lg"
+    >
+      {children}
+    </div>
+  )
+}
