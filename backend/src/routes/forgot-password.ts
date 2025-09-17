cc61ef4a94bc40d3a1601d45ad07de4dd25e3313
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-09-17 16:43:08 -0300

    feat: forgot password token generation

--- backend/src/routes/forgot-password.ts
+++ backend/src/routes/forgot-password.ts
@@ -2,6 +2,8 @@ import { FastifyInstance } from "fastify";
 import { db } from "..";
 import { users } from "../db/schema";
 import { and, eq } from "drizzle-orm";
+import { randomBytes } from "crypto";
+import dayjs from "dayjs";
 
 export async function forgotPassword(app: FastifyInstance) {
   app.post('/forgot-password', async (request: any, reply: any) => {
@@ -13,6 +15,14 @@ export async function forgotPassword(app: FastifyInstance) {
       if (!user) {
         return reply.status(404).send({ message: 'Não foi possível encontrar um usuário com esse email!' });
       }
+
+      const forgotPasswordToken = randomBytes(64).toString('hex');
+      const forgotPasswordTokenExpiry = dayjs.utc().add(15, 'minute').format();
+
+      await db.update(users).set({
+        verifyForgotPasswordToken: forgotPasswordToken,
+        verifyForgotPasswordTokenExpiry: forgotPasswordTokenExpiry,
+      }).where(eq(users.email, email));
 
       return reply.status(200).send({ message: 'Email enviado com sucesso!' });
     } catch (error) {
