386c7547a5ffc75154505cc1372770d603eae967
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-09-17 11:41:43 -0300

    feat: basic routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,15 +5,19 @@ import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Login from './Login.tsx'
 import VerifyEmail from './VerifyEmail.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      {/* <App /> */}
-      <Login />
-      {/* <VerifyEmail /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path='login' element={<Login />} />
+          <Route path='verify-email' element={<VerifyEmail />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
 )
