f8659591ee36a4d485370b37f22cad4d5cbc16d1
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-10-09 21:38:12 -0300

    feat: added try catch in db actions to hide stack trace in non human errors

--- backend/src/routes/create-user.ts
+++ backend/src/routes/create-user.ts
@@ -17,28 +17,34 @@ export async function createUser(app: FastifyInstance) {
       return reply.status(400).send({ message: 'Nome, email e senha são obrigatórios!' });
     }
 
-    const user = await db.query.users.findFirst({ where: eq(users.email, email) });
+    try {
+      const user = await db.query.users.findFirst({ where: eq(users.email, email) });
 
-    if (user && user.verified) {
-      return reply.status(409).send({ message: 'Usuário já cadastrado!' });
+      if (user && user.verified) {
+        return reply.status(409).send({ message: 'Usuário já cadastrado!' });
+      }
+
+      const verifyToken = randomBytes(64).toString('hex');
+      const verifyTokenExpiry = dayjs.utc().add(1, 'day').format();
+      const hashVerifyToken = await argon2.hash(verifyToken);
+
+      if (!user) {
+        const hashedPassword = await argon2.hash(password);
+
+        await db.insert(users).values({ name, email, password: hashedPassword, verifyToken: hashVerifyToken, verifyTokenExpiry });
+      }
+
+      if (user && !user.verified) {
+        await db.update(users).set({ verifyToken: hashVerifyToken, verifyTokenExpiry }).where(eq(users.email, user.email));
+      }
+
+      await sendVerificationEmail({ name, email, verifyToken });
+
+      return reply.status(201).send({ message: 'Usuário cadastrado com sucesso!' });
+    } catch (error) {
+      console.log(error);
+
+      return reply.status(500).send({ message: 'Ocorreu um erro ao cadastrar o usuário! Por favor, tente novamente.' });
     }
-
-    const verifyToken = randomBytes(64).toString('hex');
-    const verifyTokenExpiry = dayjs.utc().add(1, 'day').format();
-    const hashVerifyToken = await argon2.hash(verifyToken);
-
-    if (!user) {
-      const hashedPassword = await argon2.hash(password);
-
-      await db.insert(users).values({ name, email, password: hashedPassword, verifyToken: hashVerifyToken, verifyTokenExpiry });
-    }
-
-    if (user && !user.verified) {
-      await db.update(users).set({ verifyToken: hashVerifyToken, verifyTokenExpiry }).where(eq(users.email, user.email));
-    }
-
-    await sendVerificationEmail({ name, email, verifyToken });
-
-    return reply.status(201).send({ message: 'Usuário cadastrado com sucesso!' });
   })
 }
