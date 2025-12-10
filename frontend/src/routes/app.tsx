4f64d383d8d5b1604183d49936aeb51eebb2c618
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-12-10 11:00:55 -0300

    feat: authMiddleware

--- frontend/src/routes/app.tsx
+++ frontend/src/routes/app.tsx
@@ -8,31 +8,31 @@ import { userContext } from "./contexts/userContext";
 const appRoutes = [
   {
     path: '/kanban',
-    middleware: [timingMiddleware],
+    middleware: [authMiddleware],
+    loader: async () => null,
     Component: Kanban,
   },
   {
     path: '/profile',
+    middleware: [authMiddleware],
+    loader: async () => null,
     Component: Profile,
   },
   {
     path: '/new-email',
+    middleware: [authMiddleware],
+    loader: async () => null,
     Component: VerifyNewEmail,
   }
 ];
 
-async function timingMiddleware({ context }, next) {
-  const start = performance.now();
-  await next();
-  const duration = performance.now() - start;
-  console.log(`Navigation took ${duration}ms`);
-}
-
 // @ts-expect-error -- React Router ainda não exporta tipos oficiais para middleware context e temos que usar any, infelizmente.
 async function authMiddleware({ context }) {
+  console.log('oi');
+
   const user = await auth();
 
-  console.log('oi');
+  console.log(user);
 
   if (!user) {
     return redirect('/auth/login');
